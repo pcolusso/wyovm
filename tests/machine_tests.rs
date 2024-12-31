f420bbbb0d976d1fb79cc44244866943dec7fd73
Paul <paulcolusso@gmail.com>
2024-12-31 12:24:57 +1100

    integ tests

--- tests/machine_tests.rs
+++ tests/machine_tests.rs
@@ -1,15 +1,31 @@
+use std::io::Write;
+use std::rc::Rc;
+use std::cell::RefCell;
 use wyovm::Machine;
 
+struct WritableBuffer(Rc<RefCell<Vec<u8>>>);
+
+impl Write for WritableBuffer {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.0.borrow_mut().write(buf)
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        self.0.borrow_mut().flush()
+    }
+}
 
 #[test]
 fn test_hello_world() {
     let f = std::fs::File::open("samples/hello_world.obj").expect("hello world sample lost");
-    let buf = Vec::new();
+    // I feel all this ceremony is not necessary...
+    let buf = Rc::new(RefCell::new(Vec::new()));
     {
-        let mut m = Machine::new(Box::new(buf.clone()));
+        let mut m = Machine::new(Box::new(WritableBuffer(buf.clone())));
         m.load_image(f);
         m.run();
     }
-    let out = String::from_utf8_lossy(&buf);
-    assert_eq!(out, "Hello World!\n");
+    let x = buf.borrow();
+    let out = String::from_utf8_lossy(&x);
+    assert_eq!(out, "Hello, World!");
 }
